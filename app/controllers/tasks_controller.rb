64b5d9aa3f510240275f68fe0055fd2770323685
great.tower <greattower511@gmail.com>
2018-02-28 15:39:46 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
   
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -26,12 +27,10 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
-    
+
     if @task.update(task_params)
       flash[:succes] = 'Taskが正常に更新されました'
       redirect_to @task
@@ -43,11 +42,14 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:succes] = "正常に削除されました"
     redirect_to tasks_url
+  end
+  
+  def set_task
+    @task = Task.find(params[:id])
   end
   
   def task_params
